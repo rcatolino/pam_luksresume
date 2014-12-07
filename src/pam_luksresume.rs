ab36084f15db444e7550e5c5ef69a4fa4d3ae03a
Raphael Catolino <raphael.catolino@gmail.com>
2014-12-07 18:32:00 +0100

    Remove use of libstd.

--- src/pam_luksresume.rs
+++ src/pam_luksresume.rs
@@ -1,11 +1,19 @@
+#![feature(globs)]
+#![no_std]
+#![feature(lang_items)]
+
 extern crate libc;
+extern crate core;
 
 use libc::{c_uint, size_t};
 use pam_modules::{PamHandle, PamResult};
 mod pam_modules;
 
-/*PAM_EXTERN int pam_sm_open_session(PamHandle *pamh, int flags, argc, argv);
-*/
+// provided by libstd.
+#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
+
 #[no_mangle]
 #[allow(unused_variables)]
 pub fn pam_sm_open_session(pamh: PamHandle, flags: c_uint, argc: size_t, argv: *const u8) -> PamResult {
