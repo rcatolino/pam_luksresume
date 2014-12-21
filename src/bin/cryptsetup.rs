d3dc07bd4eb68a17a8eb6ca3415bbc833a70c9b5
Raphael Catolino <raphael.catolino@gmail.com>
2014-12-21 17:09:12 +0100

    Cleaning up cryptsetup interface.

--- src/bin/cryptsetup.rs
+++ src/bin/cryptsetup.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
 
-use libc::{c_char, c_int, c_void, size_t};
+use libc::{STDOUT_FILENO, c_char, c_int, c_void, size_t, strlen, write};
+use std::c_str::CString;
+use std::ptr;
 
-pub type CryptDevice = *mut c_int;
+pub type InnerCryptDevice = *mut c_int;
 #[repr(C)]
 pub struct CryptParamsPlain {
     pub hash: *const c_char,
@@ -34,37 +36,99 @@ pub enum CryptStatusInfo {
 
 #[link(name="cryptsetup")]
 extern "C" {
-    pub fn crypt_init(cd: *mut CryptDevice, device: *const c_char) -> c_int;
-    pub fn crypt_init_by_name_and_header(cd: *mut CryptDevice,
+    pub fn crypt_init(cd: *mut InnerCryptDevice, device: *const c_char) -> c_int;
+    pub fn crypt_init_by_name_and_header(cd: *mut InnerCryptDevice,
                                          name: *const c_char,
                                          header_device: *const c_char) -> c_int;
-    pub fn crypt_init_by_name(cd: *mut CryptDevice, name: *const c_char) -> c_int;
-    pub fn crypt_set_log_callback(cd: CryptDevice,
+    pub fn crypt_init_by_name(cd: *mut InnerCryptDevice, name: *const c_char) -> c_int;
+    pub fn crypt_set_log_callback(cd: InnerCryptDevice,
                                   log: extern "C" fn (lvl: c_int, msg: *const c_char,
                                                       usrptr: *const c_void),
                                   usrptr: *const c_void);
-    pub fn crypt_log(cd: CryptDevice, level: c_int, msg: *const u8);
-    pub fn crypt_set_iterarion_time(cd: CryptDevice, iteration_time_ms: u64);
-    pub fn crypt_set_data_device(cd: CryptDevice, device: *const c_char) -> c_int;
-    pub fn crypt_memory_lock(cd: CryptDevice, lock: c_int) -> c_int;
-    pub fn crypt_get_type(cd: CryptDevice) -> *const c_char;
-    pub fn crypt_load(cd: CryptDevice, requested_type: *const c_char,
+    pub fn crypt_log(cd: InnerCryptDevice, level: c_int, msg: *const u8);
+    pub fn crypt_set_iterarion_time(cd: InnerCryptDevice, iteration_time_ms: u64);
+    pub fn crypt_set_data_device(cd: InnerCryptDevice, device: *const c_char) -> c_int;
+    pub fn crypt_memory_lock(cd: InnerCryptDevice, lock: c_int) -> c_int;
+    pub fn crypt_get_type(cd: InnerCryptDevice) -> *const c_char;
+    pub fn crypt_load(cd: InnerCryptDevice, requested_type: *const c_char,
                       params: *const c_void) -> c_int;
-    pub fn crypt_suspend(cd: CryptDevice, name: *const c_char) -> c_int;
-    pub fn crypt_resume_by_passphrase(cd: CryptDevice,
+    pub fn crypt_suspend(cd: InnerCryptDevice, name: *const c_char) -> c_int;
+    pub fn crypt_resume_by_passphrase(cd: InnerCryptDevice,
                                       name: *const c_char,
                                       keyslot: c_int,
                                       passphrase: *const c_char,
                                       passphrase_size: size_t) -> c_int;
-    pub fn crypt_free(cd: CryptDevice);
-    pub fn crypt_volume_key_verify(cd: CryptDevice,
+    pub fn crypt_free(cd: InnerCryptDevice);
+    pub fn crypt_volume_key_verify(cd: InnerCryptDevice,
                                    volume_key: *const c_char,
                                    volume_key_size: size_t) -> c_int;
-    pub fn crypt_status(cd: CryptDevice,
+    pub fn crypt_status(cd: InnerCryptDevice,
                         name: *const c_char) -> CryptStatusInfo;
-    pub fn crypt_dump(cd: CryptDevice) -> c_int;
-    pub fn crypt_get_uuid(cd: CryptDevice) -> *const c_char;
-    pub fn crypt_get_device_name(cd: CryptDevice) -> *const c_char;
+    pub fn crypt_dump(cd: InnerCryptDevice) -> c_int;
+    pub fn crypt_get_uuid(cd: InnerCryptDevice) -> *const c_char;
+    pub fn crypt_get_device_name(cd: InnerCryptDevice) -> *const c_char;
     pub fn crypt_get_dir() -> *const c_char;
     pub fn crypt_set_debug_level(level: c_int);
+}
+
+#[no_mangle]
+#[allow(unused_variables)]
+pub extern "C" fn crypt_log_cb(level: c_int, msg: *const c_char, _: *const c_void) {
+    unsafe {
+        write(STDOUT_FILENO, msg as *const c_void, strlen(msg));
+    }
+}
+
+pub struct CryptDevice {
+    name: CString,
+    indev: InnerCryptDevice,
+}
+
+impl CryptDevice {
+    pub fn new_by_name(dev_name: &str) -> Option<CryptDevice> {
+        let mut crypt_dev: InnerCryptDevice = ptr::null_mut();
+        let name = dev_name.to_c_str();
+        if unsafe { crypt_init_by_name(&mut crypt_dev, name.as_ptr()) != 0 } {
+            None
+        } else {
+            unsafe {
+                crypt_set_log_callback(crypt_dev, crypt_log_cb, ptr::null());
+            }
+            Some(CryptDevice {
+                name: name,
+                indev: crypt_dev,
+            })
+        }
+    }
+
+    pub fn set_debug(enabled: bool) {
+        unsafe {
+            if enabled {
+                crypt_set_debug_level(2 as c_int);
+            } else {
+                crypt_set_debug_level(0 as c_int);
+            }
+        }
+    }
+
+    pub fn luks_load(&self) -> bool {
+        unsafe {
+            crypt_load(self.indev, b"LUKS1\0".as_ptr() as *const c_char,
+                       ptr::null()) == 0
+        }
+    }
+
+    pub fn resume(&self, pass: &str) -> bool {
+        unsafe {
+            crypt_resume_by_passphrase(self.indev, self.name.as_ptr(),
+                                       -1 as c_int, pass.as_ptr() as *const c_char,
+                                       pass.len() as u64) >= 0
+
+        }
+    }
+
+    pub fn free(self) {
+        println!("Freeing crypt device");
+        unsafe { crypt_free(self.indev) };
+    }
 }
--- src/bin/pam_luksresume_helper.rs
+++ src/bin/pam_luksresume_helper.rs
@@ -1,28 +1,16 @@
 
 extern crate libc;
 
-use std::ptr;
 use std::os;
 use std::io;
-use libc::{c_char, c_void, c_int, strlen, STDOUT_FILENO, write, setuid};
-use cryptsetup::{CryptDevice, crypt_init_by_name, crypt_free, crypt_set_log_callback,
-                 crypt_load, crypt_set_debug_level,
-                 crypt_resume_by_passphrase};
+use libc::setuid;
+use cryptsetup::CryptDevice;
 mod cryptsetup;
 
-#[no_mangle]
-#[allow(unused_variables)]
-pub extern "C" fn crypt_log_cb(level: c_int, msg: *const c_char, _: *const c_void) {
-    unsafe {
-        write(STDOUT_FILENO, msg as *const c_void, strlen(msg));
-    }
-}
-
 fn main() {
-    let mut crypt_dev: CryptDevice = ptr::null_mut();
     let args = os::args();
     let dev_name = match args.get(1) {
-        Some(name) => name.to_c_str(),
+        Some(name) => name,
         None => {
             println!("Error, usage : pam_luksresume_helper <device name>");
             os::set_exit_status(-1);
@@ -30,48 +18,37 @@ fn main() {
         }
     };
 
-    let ret = if unsafe { setuid(0) == -1 } {
-        println!("Error, not running as setuid root");
-        -1i
-    } else if unsafe { crypt_init_by_name(&mut crypt_dev, dev_name.as_ptr()) != 0 } {
-        println!("Error, invalid device {}", dev_name);
-        -2i
+    let crypt_dev = if unsafe { setuid(0) == -1 } {
+        Err((1i, String::from_str("Error, not running as setuid root")))
     } else {
-        unsafe {
-            crypt_set_log_callback(crypt_dev, crypt_log_cb, ptr::null());
-            crypt_set_debug_level(2 as c_int);
-        }
-
-        let ret = if unsafe { crypt_load(crypt_dev, b"LUKS1\0".as_ptr() as *const c_char,
-                                ptr::null()) != 0 } {
-            println!("Device load failed");
-            -3i
-        } else {
-            match io::stdin().read_line() {
-                Ok(inpass) => {
-                    let pass = inpass.trim_right_chars('\n');
-                    println!("Using password {}", pass);
-                    if unsafe { crypt_resume_by_passphrase(crypt_dev,
-                                                 dev_name.as_ptr(),
-                                                 -1 as c_int,
-                                                 pass.as_ptr() as *const c_char,
-                                                 pass.len() as u64) < 0  } {
-                        println!("Error resuming volume");
-                        -4i
-                    } else {
-                        0i
-                    }
-                }
-                Err(err) => {
-                    println!("Error reading passphrase: {}", err);
-                    -5i
-                }
-            }
-        };
-        unsafe { crypt_free(crypt_dev) };
-        ret
+        CryptDevice::new_by_name(dev_name.as_slice())
+        .ok_or((2i, format!("Error, invalid device name : {}", dev_name)))
     };
 
-    os::set_exit_status(ret);
+    let result = crypt_dev.and_then(|cd| {
+        CryptDevice::set_debug(true);
+        if cd.luks_load() {
+            Ok(cd)
+        } else {
+            Err((3i, String::from_str("Device load failed")))
+        }
+    }).and_then(|cd| {
+        match io::stdin().read_line() {
+            Ok(inpass) => if cd.resume(inpass.trim_right_chars('\n')) {
+                Ok(())
+            } else {
+                Err((4i, String::from_str("Error resuming volume")))
+            },
+            Err(err) => Err((5i, format!("Error reading passphrase: {}", err))),
+        }
+    }).err();
+
+    match result {
+        None => os::set_exit_status(0),
+        Some((ret, err)) => {
+            println!("{}", err);
+            os::set_exit_status(ret)
+        }
+    }
 }
 
