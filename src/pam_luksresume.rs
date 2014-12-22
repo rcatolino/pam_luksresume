8fa43306848aae72ac557421016aaa25b25c9b1f
Raphael Catolino <raphael.catolino@gmail.com>
2014-12-22 20:17:36 +0100

    Using pam arguments instead of hardcoded values. Working prototype.

--- src/pam_luksresume.rs
+++ src/pam_luksresume.rs
@@ -11,6 +11,7 @@ use std::intrinsics::transmute;
 use std::prelude::*;
 use std::ptr;
 use std::slice::from_raw_buf;
+use std::str::from_utf8;
 use libc::{c_char, c_void, c_int, c_uint, pid_t, size_t, strlen};
 use pam_modules::{PamConv, PamItemType, PamHandle, PamMessage, PamMsgStyle, PamResponse,
                   PamResult, pam_get_item, syslog, printf};
@@ -68,37 +69,51 @@ fn get_password<'a>(pamh: PamHandle) -> Result<&'a mut PamResponse, &'static str
     })
 }
 
-fn try_resume(pass: &PamResponse) -> Result<bool, &'static str> {
-    // TODO: remove hardcoded strings.
-    let mut cmd = Command::new("/home/raph/usr/local/lib/pam_luksresume/pam_luksresume_helper");
-    cmd.env_set_all([("USER", "")].as_slice());
+fn try_resume(pass: &PamResponse, helper_path: &str,
+              dev_name: &str) -> Result<bool, &'static str> {
+    let mut cmd = Command::new(helper_path);
+    cmd.env_set_all([("", "")].as_slice());
     cmd.stdout(InheritFd(1 as c_int));
-    cmd.arg("_dev_sda4");
-    match cmd.spawn().and_then(|mut process| {
+    cmd.arg(dev_name);
+    cmd.spawn().and_then(|mut process| {
         process.stdin.as_mut().map(|mut pipe| {
             let ref_ptr = &(pass.get_buff() as *const u8);
             unsafe {
-                pipe.write(from_raw_buf(ref_ptr, strlen(pass.get_buff()) as uint));
+                pipe.write(from_raw_buf(ref_ptr, strlen(pass.get_buff()) as uint))
             }
         });
         process.wait()
-    }) {
-        Ok(status) => {
-            Ok(status.success())
-        },
-        Err(ioerr) => Err(ioerr.desc),
-    }
+    }).map(|status| status.success()).map_err(|ioerr| ioerr.desc)
 }
 
 #[no_mangle]
 #[allow(unused_variables)]
 pub extern "C" fn pam_sm_authenticate(pamh: PamHandle, flags: c_uint,
-                                      argc: size_t, argv: *const u8) -> PamResult {
+                                      argc: size_t, argv: *const *const u8) -> PamResult {
     syslog(pamh, "In pam_sm_authenticate");
+    if argc < 2 {
+        syslog(pamh,
+               "Missing argument, usage : pam_luksresume.so <path to helper> <device name>");
+        return PamResult::SERVICE_ERR;
+    }
+
+    let (helper, dev_name) = unsafe {
+        let args = from_raw_buf(&argv, argc as uint);
+        let mbh = from_utf8(from_raw_buf(&args[0], strlen(args[0] as *const c_char) as uint));
+        let mbn = from_utf8(from_raw_buf(&args[1], strlen(args[1] as *const c_char) as uint));
+        match (mbh, mbn) {
+            (Some(h), Some(n)) => (h, n),
+            _ => {
+                syslog(pamh, "Error, arguments must be utf8 encoded");
+                return PamResult::SERVICE_ERR;
+            },
+        }
+    };
+
     match get_password(pamh) {
         Ok(pass) => unsafe {
             printf(b"Got a password : %s\n".as_ptr(), pass.get_buff());
-            match try_resume(pass) {
+            match try_resume(pass, helper, dev_name) {
                 Err(msg) => syslog(pamh, msg),
                 Ok(true) => syslog(pamh, "Successfullness!!!!"),
                 Ok(false) => syslog(pamh, "Failed to authenticate"),
